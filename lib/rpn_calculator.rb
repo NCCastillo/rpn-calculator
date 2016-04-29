a960906554d30d82df107bf9a830c9e86335632b
Nestor Castillo <nes_23@yahoo.com>
2016-04-28 18:22:29 -0600

    Slight refactor
    come up with more descriptive names and extract out into methods

--- lib/rpn_calculator.rb
+++ lib/rpn_calculator.rb
@@ -1,37 +1,35 @@
 class RpnCalculator
   def initialize
-    @input_store = []
-    @rpn_store = []
+    @rpn_stack = []
   end
 
   def evaluate(input)
-    input_store << input
-
-    input_store.each do |item|
-      case item
-        when /[0-9]/
-          rpn_store << item.to_f
-        when "+", "-", "*", "/"
-          calculate_and_add_to_store(item)
-        else
-          raise "Operation cannot be performed"
-      end
+    case input
+      when /[0-9]/
+        add_input_to_stack(input)
+      when "+", "-", "*", "/"
+        calculate_and_add_to_stack(input)
+      else
+        raise "Operation cannot be performed"
     end
 
-    input_store.clear
-    rpn_store.last
   end
 
   private
 
-  attr_reader :input_store, :rpn_store
+  attr_reader :rpn_stack
 
-  def calculate_and_add_to_store(operator)
-    raise "Operation cannot be performed" if rpn_store.size < 2
+  def calculate_and_add_to_stack(operator)
+    raise "Operation cannot be performed" if rpn_stack.size < 2
 
-    value_2 = rpn_store.pop
-    value_1 = rpn_store.pop
+    value_2 = rpn_stack.pop
+    value_1 = rpn_stack.pop
 
-    rpn_store << value_1.send(operator, value_2)
+    rpn_stack << value_1.send(operator, value_2)
+  end
+
+  def add_input_to_stack(input)
+    rpn_stack << input.to_f
+  end
   end
 end
